814b8b1f868d61a8c52ab3ec8275e103728ab282
Hunter Read <readh2@wwu.edu>
2018-11-03 21:16:55 -0700

    Major updates to heatmap with interactivity

--- js/application.js
+++ js/application.js
@@ -37,6 +37,9 @@ const legendId = 'legendGradient';
 const stable_color = "#2c7bb6";
 const middle_color = "#ffffbf";
 const unstable_color = "#d7191c";
+const low_color = "#e5f5f9";
+const mid_color = "#99d8c9";
+const high_color = "#2ca25f";
 const black = "#000";
 const white = "#fff";
 
@@ -46,7 +49,31 @@ const y = d3.scale.ordinal().rangeBands([0, height]);
 const z = d3.scale.linear().range([0, 1]).clamp(true);
 const c = d3.scale.linear().range([unstable_color, middle_color, stable_color]);
 
+//options nemu
+let selected_option = "rigidityDistance";
+const options_data = {
+  "Rigidity Distance": "rigidityDistance",
+  "Average Cluster Size": "avgClusterSize",
+  "Largest Cluster": "largestCluster"
+};
 
+const select = d3.select('#options-menu')
+  .append('select')
+  	.attr('class','select')
+    .on('change', onOptionChange);
+
+const options = select
+  .selectAll('option')
+	.data(Object.keys(options_data)).enter()
+	.append('option')
+		.text((d) => d);
+
+function onOptionChange() {
+	selected_option = options_data[d3.select('select').property('value')]
+	updateHeatmap();
+}
+
+//svg initialize
 const svg = d3.select("#heatmap")
   .append("svg")
   .attr("width", width + margin.left + margin.right)
@@ -76,17 +103,17 @@ const gradient = svg.append("svg:defs")
   .attr("y2", "0%")
   .attr("spreadMethod", "pad");
 
-gradient.append("svg:stop")
+const leftGradient = gradient.append("svg:stop")
   .attr("offset", "0%")
   .attr("stop-color", unstable_color)
   .attr("stop-opacity", 1);
 
-gradient.append("svg:stop")
+const midGradient = gradient.append("svg:stop")
   .attr("offset", "50%")
   .attr("stop-color", middle_color)
   .attr("stop-opacity", 1);
 
-gradient.append("svg:stop")
+const rightGradient = gradient.append("svg:stop")
   .attr("offset", "100%")
   .attr("stop-color", stable_color)
   .attr("stop-opacity", 1);
@@ -97,19 +124,24 @@ legend.append("svg:rect")
   .style("fill", "url(#heatmap-gradient)")
   .attr("transform", `translate(${margin.left}, 0)`);
 
-legend.append("text")
+const rightLegendText = legend.append("text")
   .attr("class", "legendText")
   .attr("text-anchor", "end")
   .attr("y", legendHeight - 5)
   .attr("x", legendWidth - margin.right - 10)
+  .style("fill", black)
   .text("Stable");
 
-legend.append("text")
+const leftLegendText = legend.append("text")
   .attr("class", "legendText")
   .attr("y", legendHeight - 5)
   .attr("x", margin.left + 10)
   .style("fill", white)
   .text("Unstable");
+
+const tooltip = d3.select("body").append("div")
+    .attr("class", "tooltip")
+    .style("opacity", 0);
 
 
 let proteinData = [];
@@ -162,20 +194,6 @@ d3.json("data/1KJ5.json", (data) => {
     .attr("transform", (d, i) => `translate(0, ${y(i)})`)
     .each(rowFunc);
 
-  function rowFunc(rowObject) {
-    let cell = d3.select(this).selectAll(".cell")
-      .data(rowObject)
-      .enter().append("rect")
-      .attr("class", "cell")
-      .attr("x", (d, i) => x(i))
-      .attr("width", x.rangeBand())
-      .attr("height", y.rangeBand())
-      // .style("fill-opacity", (d) => {
-      //   return d !== null ? z(d.rigidityDistance) : 1;
-      // })
-      .style("fill", (d) => d !== null ? c(d.rigidityDistance) : black);
-  }
-
   //row labels
   row.append("line")
   .attr("x2", width)
@@ -208,3 +226,75 @@ d3.json("data/1KJ5.json", (data) => {
     .text((d, i) =>  acid_labels[i]);
 
 });
+
+function updateHeatmap() {
+  let min = d3.min(proteinData, (d) => d3.min(d, (k) => k ? k[selected_option] : Infinity));
+  let max = d3.max(proteinData, (d) => d3.max(d, (k) => k ? k[selected_option] : -Infinity));
+  if (selected_option === "rigidityDistance") {
+    c.range([unstable_color, middle_color, stable_color]).domain([min, 0, max]);
+
+    leftLegendText.style("fill", white)
+      .text("Unstable");
+    rightLegendText.style("fill", black)
+      .text("Stable");
+
+    leftGradient.attr("stop-color", unstable_color);
+    midGradient.attr("stop-color", middle_color);
+    rightGradient.attr("stop-color", stable_color);
+  } else {
+    c.range([low_color, mid_color, high_color]).domain([min, max]);
+
+    leftLegendText.style("fill", black)
+      .text(min);
+    rightLegendText.style("fill", white)
+      .text(max);
+
+    leftGradient.attr("stop-color", low_color);
+    midGradient.attr("stop-color", mid_color);
+    rightGradient.attr("stop-color", high_color);
+  }
+  svg.selectAll(".row")
+    .data(proteinData)
+    .each(rowFuncUpdate);
+
+  leftLegendText.text(min);
+}
+
+function rowFuncUpdate(rowObject) {
+  let cell = d3.select(this).selectAll(".cell")
+    .data(rowObject)
+    .transition().duration(1000)
+    .style("fill", (d) => d !== null ? c(d[selected_option]) : black);
+}
+
+function rowFunc(rowObject, idx) {
+  let cell = d3.select(this).selectAll(".cell")
+    .data(rowObject)
+    .enter().append("rect")
+    .attr("class", "cell")
+    .attr("x", (d, i) => x(i))
+    .attr("width", x.rangeBand())
+    .attr("height", y.rangeBand())
+    .style("fill", (d) => d !== null ? c(d[selected_option]) : black)
+    .on("mouseover", (d, i) => {
+      let index = rowObject.indexOf(null);
+      tooltip.transition()
+          .duration(200)
+          .style("opacity", 0.9);
+      if (d) {
+        tooltip.html(`Amino Acid ${idx + 1}<br/>From ${acid_labels[index]} to ${acid_labels[i]}<br\>
+          Rigidity Distance: ${d.rigidityDistance}<br/>Average Cluster Size: ${d.avgClusterSize}<br/>Largest Cluster: ${d.largestCluster}`)
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
+      } else {
+        tooltip.html("<br/><br/>WT amino acid")
+          .style("left", (d3.event.pageX) + "px")
+          .style("top", (d3.event.pageY - 28) + "px");
+      }
+    })
+    .on("mouseout", function(d) {
+        tooltip.transition()
+            .duration(500)
+            .style("opacity", 0);
+    });
+}
